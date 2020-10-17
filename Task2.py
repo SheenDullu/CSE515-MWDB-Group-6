cd924c902ca1504537f4adeea93f838f7eeb49ec
cruter <43527634+cruter@users.noreply.github.com>
2020-10-17 10:50:14 -0700

    Edit Distance Task 2

--- Task2.py
+++ Task2.py
@@ -1,5 +1,7 @@
+import glob
+import re
 import pandas as pd
-
+import numpy as np
 import Utilities
 
 
@@ -19,6 +21,54 @@ def dotProduct(directory, file, model):
     printTop10Values(dot_product[:11])
 
 
+def editDistanceFunc(P, Q):
+    replaceCost = 1
+    insertCost = 1
+    deleteCost = 1
+    if len(P) < len(Q):
+        P, Q, = Q, P
+    i = len(P) + 1
+    j = len(Q) + 1
+    matrix = np.zeros((i, j))
+    for x in range(i):
+        matrix[x, 0] = x*insertCost
+    for y in range(j):
+        matrix[0, y] = y*insertCost
+
+    for x in range(1, i):
+        for y in range(1, j):
+            if P[x-1] == Q[y-1]:
+                matrix[x, y] = matrix[x-1, y-1]
+            else:
+                matrix[x, y] = min(matrix[x-1, y] + insertCost, matrix[x, y-1] + deleteCost, matrix[x-1, y-1] +
+                                   replaceCost)
+    return matrix[i - 1, j - 1]
+
+def editDistance(directory, file):
+    wrdfile = Utilities.fetchWordsFromFile(directory, file)
+    allwrdfiles = Utilities.fetchAllWordsFromFile(directory)
+
+    all_files = glob.glob(directory + "/*.wrd")
+    all_files.sort(key=lambda var: [int(x) if x.isdigit() else x for x in re.split('(\d+)', var)])
+
+    editValues = []
+    for file in allwrdfiles:
+        editVal = 0
+        for key in wrdfile:
+            editVal = 0
+            numArr = []
+            numArr2 = []
+            for wrd in wrdfile[key]:
+                numArr.extend(list(map(int, wrd.split(" "))))
+            for wrd in file[key]:
+                numArr2.extend(list(map(int, wrd.split(" "))))
+            editVal += editDistanceFunc(numArr, numArr2)
+        editValues.append(editVal)
+    vals = sorted(range(len(editValues)), key=lambda k: editValues[k])
+
+    for i in range(0, 11):
+        print(str(i+1) + " . Gesture " + all_files[vals[i]].split("\\")[-1].split(".")[0] + " , " + str(editValues[vals[i]]))
+
 def main():
     while True:
         # print("Top 10 most Similar Gesture")
@@ -34,6 +84,9 @@ def main():
         # print('Enter 7 for DTW')
         task_input = input("What Task  do you want to perform: (enter 0 to exit)\n")
         task = int(task_input)
+        if task == 6:
+            editDistance(r'C:\Class\CSE515 Multimedia\3_class_gesture_data', '1')
+            print("########## Completed Edit Distance ##########")
         if task == 1:
             # dotProduct(directory, gesture, vector_model)
             dotProduct(r'D:\ASU\Courses\MWDB\Project\Phase 2\Code\data', '1', 'tf')
--- Utilities.py
+++ Utilities.py
@@ -1,5 +1,6 @@
 import glob
 import os
+import re
 
 
 def get_all_sub_folders(folder_directory):
@@ -45,6 +46,45 @@ def getAVector(file):
     vector_string = f.split(',')
     vector = [float(i) for i in vector_string]
     return vector
+
+def fetchWordsFromFile(directory, filename):
+    wordDict = {}
+    words = []
+    file = directory + "/" + filename + ".wrd"
+    with open(file, 'r') as f:
+        for line in f:
+            row = line.strip().split(',')
+            word = ' '.join(map(str, row[0:3]))
+            key = row[0] + row[1]
+            wrd = row[2]
+            if key in wordDict:
+                wordDict[key].append(wrd)
+            else:
+                wrdlist = []
+                wrdlist.append(wrd)
+                wordDict[key] = wrdlist
+    return wordDict
+
+def fetchAllWordsFromFile(directory):
+    wordArrayDict = []
+    all_files = glob.glob(directory + "/*.wrd")
+    all_files.sort(key=lambda var: [int(x) if x.isdigit() else x for x in re.split('(\d+)', var)])
+    for filename in all_files:
+        wordDict = {}
+        with open(filename, 'r') as f:
+            for line in f:
+                row = line.strip().split(',')
+                word = ' '.join(map(str, row[0:3]))
+                key = row[0] + row[1]
+                wrd = row[2]
+                if key in wordDict:
+                    wordDict[key].append(wrd)
+                else:
+                    wrdlist = []
+                    wrdlist.append(wrd)
+                    wordDict[key] = wrdlist
+        wordArrayDict.append(wordDict)
+    return wordArrayDict
 
 
 if __name__ == '__main__':
