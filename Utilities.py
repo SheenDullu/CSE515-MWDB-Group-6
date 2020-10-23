11977eed1ca74508cc80379dbd10cca378215efa
Sheen <sheendullu13@gmail.com>
2020-10-23 01:44:13 -0700

    Minor changes

--- Utilities.py
+++ Utilities.py
@@ -1,6 +1,7 @@
 import glob
 import os
 import re
+
 import numpy as np
 
 
@@ -41,12 +42,12 @@ def getAllVectors(directory, model):
 
 
 def getAVector(file):
-    # file_name = file.split("\\")[-1].split(".")[0]
     file = open(file, 'r')
     f = file.read()
     vector_string = f.split(',')
     vector = [float(i) for i in vector_string]
     return vector
+
 
 def fetchWordsFromFile(directory, filename):
     wordDict = {}
@@ -88,17 +89,16 @@ def fetchAllWordsFromFile(directory):
         wordArrayDict.append(wordDict)
     return wordArrayDict
 
-def editDistanceComp(matrix,p,q,P,Q,r,i,d,con):
+
+def editDistanceComp(matrix, p, q, P, Q, r, i, d, con):
     replaceCost = 2
     insertCost = 1
     deleteCost = 1
 
-
-    if (p==0 or q==0):
-
+    if (p == 0 or q == 0):
         return 0
 
-    #Commented out the below code so that I can test the cost function. Can optimise the code later.
+    # Commented out the below code so that I can test the cost function. Can optimise the code later.
     # if(i+d>0.3*con):
     #     # print("id",i,d,0.3*con)
     #     return 1000
@@ -107,44 +107,51 @@ def editDistanceComp(matrix,p,q,P,Q,r,i,d,con):
     #     # print('r',r)
     #     return 1000
 
-    if(P[p]== Q[q]):
-        if (matrix[p-1,q-1] == -1):
-            matrix[p-1,q-1]=editDistanceComp(matrix,p-1,q-1,P,Q,r,i,d,con)
-        return matrix[p-1,q-1]
+    if (P[p] == Q[q]):
+        if (matrix[p - 1, q - 1] == -1):
+            matrix[p - 1, q - 1] = editDistanceComp(matrix, p - 1, q - 1, P, Q, r, i, d, con)
+        return matrix[p - 1, q - 1]
 
-    if(matrix[p,q-1]==-1):
+    if (matrix[p, q - 1] == -1):
+        matrix[p, q - 1] = editDistanceComp(matrix, p, q - 1, P, Q, r, i, d + 1, con)
 
-        matrix[p,q-1]=editDistanceComp(matrix,p,q-1,P,Q,r,i,d+1,con)
-
-    if(matrix[p-1,q-1]==-1):
-
-        matrix[p-1,q-1]=editDistanceComp(matrix,p-1,q-1,P,Q,r+1,i,d,con)
-    if(matrix[p-1,q]==-1):
-
-
-        matrix[p-1,q]=editDistanceComp(matrix,p-1,q,P,Q,r,i+1,d,con)
+    if (matrix[p - 1, q - 1] == -1):
+        matrix[p - 1, q - 1] = editDistanceComp(matrix, p - 1, q - 1, P, Q, r + 1, i, d, con)
+    if (matrix[p - 1, q] == -1):
+        matrix[p - 1, q] = editDistanceComp(matrix, p - 1, q, P, Q, r, i + 1, d, con)
 
     # print(r,i,d)
-    return min(matrix[p-1, q] + insertCost,matrix[p, q-1] + deleteCost, matrix[p-1, q-1] + 1)
+    return min(matrix[p - 1, q] + insertCost, matrix[p, q - 1] + deleteCost, matrix[p - 1, q - 1] + 1)
 
-def editDistanceFunc(P,Q):
 
+def editDistanceFunc(P, Q):
     if len(P) < len(Q):
         P, Q, = Q, P
     i = len(P)
     j = len(Q)
 
-    con=(max(i,j))
-    matrix = np.ones((i, j))*-1
+    con = (max(i, j))
+    matrix = np.ones((i, j)) * -1
 
-    matrix[:, 0] = np.arange(0,i)
-    matrix[0,:] = np.arange(0,j)
+    matrix[:, 0] = np.arange(0, i)
+    matrix[0, :] = np.arange(0, j)
+
+    matrix[i - 1, j - 1] = editDistanceComp(matrix, i - 1, j - 1, P, Q, 0, 0, 0, con)
+
+    return matrix[i - 1, j - 1]
 
 
-    matrix[i-1,j-1] = editDistanceComp(matrix,i-1,j-1,P,Q,0,0,0,con)
+def store_directory(directory):
+    with open("directory.txt", 'w') as f:
+        f.write(directory)
+        f.close()
 
 
-    return matrix[i-1,j-1]
+def read_directory():
+    with open("directory.txt", 'r') as f:
+        param = f.read()
+        f.close()
+    return param
 
 
 if __name__ == '__main__':
