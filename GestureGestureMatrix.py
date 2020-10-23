8db1486162c6db2d286b85483fe7e39bf8cb28f1
Sheen <sheendullu13@gmail.com>
2020-10-23 01:41:02 -0700

    Minor changes

--- GestureGestureMatrix.py
+++ GestureGestureMatrix.py
@@ -1,5 +1,6 @@
 import glob
 import re
+
 import numpy as np
 import pandas as pd
 
@@ -11,6 +12,7 @@ def dotProduct(directory):
     dot_product = all_vectors.dot(all_vectors.T)
     return dot_product
 
+
 def editDistance(directory):
     allwrdfiles = Utilities.fetchAllWordsFromFile(directory)
 
@@ -18,9 +20,9 @@ def editDistance(directory):
     all_files.sort(key=lambda var: [int(x) if x.isdigit() else x for x in re.split('(\d+)', var)])
 
     important_sensors = set([5, 6, 7, 8, 9, 10, 11, 12])
-    editValueMatrix = np.zeros([len(allwrdfiles)-1, len(allwrdfiles)-1], dtype=int)
-    for i in range(len(allwrdfiles)-1):
-        for j in range(i+1, len(allwrdfiles)-1):
+    editValueMatrix = np.zeros([len(allwrdfiles) - 1, len(allwrdfiles) - 1], dtype=int)
+    for i in range(len(allwrdfiles) - 1):
+        for j in range(i + 1, len(allwrdfiles) - 1):
             editVal = 0
             for key in allwrdfiles[i]:
                 # if (counter >30 and counter<60):
@@ -47,6 +49,7 @@ def editDistance(directory):
         print("File " + all_files[i].split("\\")[-1].split(".")[0] + " done")
     np.save('editDistanceMatrix.npy', editValueMatrix)
 
+
 if __name__ == '__main__':
-    #dotProduct(r'D:\ASU\Courses\MWDB\Project\Phase 2\Code\data')
+    # dotProduct(r'D:\ASU\Courses\MWDB\Project\Phase 2\Code\data')
     editDistance(r'C:\Class\CSE515 Multimedia\3_class_gesture_data')
